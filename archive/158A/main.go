137a4bb0ddc0add99eab24fd87d0f57550c95248
kipitix <kipitix@vk.com>
2026-06-04 17:09:14 +0300

    task 158A

--- archive/158A/main.go
+++ archive/158A/main.go
@@ -2,17 +2,54 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"os"
+	"strconv"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(nextRoundPlayersCount(os.Stdin))
+}
+
+func nextRoundPlayersCount(reader io.Reader) int {
+	result := 0
+
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	k, _ := strconv.Atoi(scanner.Text())
+
+	lastScore := 0
+	for i := 0; i < k; i++ {
+		if scanner.Scan() {
+			if x, _ := strconv.Atoi(scanner.Text()); x > 0 {
+				result++
+				lastScore = x
+			}
+		}
+	}
+
+	if lastScore == 0 {
+		return result
+	}
+
+	for i := k; i < n; i++ {
+		if scanner.Scan() {
+			if x, _ := strconv.Atoi(scanner.Text()); x == lastScore {
+				result++
+			} else {
+				break
+			}
+		}
+	}
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-}
 
-func nextRoundPlayersCount(string) {
-
+	return result
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kipitix/code_forces_souls
+
+go 1.26.3
