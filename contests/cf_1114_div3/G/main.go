fd1c9501fcefb4cc60d900f69ec587b1551d34b7
kipitix <kipitix@vk.com>
2026-09-07 10:00:10 +0300

    save work about G task

--- contests/cf_1114_div3/G/main.go
+++ contests/cf_1114_div3/G/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -44,53 +45,20 @@ func main() {
 	}
 }
 
-type Node struct {
-	index    int
-	weight   int
-	children []*Node
-	parent   *Node
-	group    *Group
-}
+// Поиск решения кейса.
+func Solve(root *Node) []int {
+	root.PrintTree()
+	groups := InitGroups(root)
 
-func (n *Node) PrintTree() {
-	if n == nil {
-		fmt.Println("Tree is nil")
-		return
-	}
-	n.printTreeDetailed("", true)
-}
-
-func (n *Node) printTreeDetailed(prefix string, isLast bool) {
-	// Определяем символы для визуализации
-	var connector, childPrefix string
-	if isLast {
-		connector = "└── "
-		childPrefix = prefix + "    "
-	} else {
-		connector = "├── "
-		childPrefix = prefix + "│   "
+	for _, g := range groups {
+		g.PrintGroup()
 	}
 
-	// Выводим информацию об узле
-	parentInfo := "nil"
-	if n.parent != nil {
-		parentInfo = fmt.Sprintf("%d", n.parent.index)
-	}
-
-	fmt.Printf("%s%s %d (weight=%d, parent=%s, children=%d)\n",
-		prefix, connector, n.index, n.weight, parentInfo, len(n.children))
-
-	// Рекурсивно выводим детей
-	for i, child := range n.children {
-		child.printTreeDetailed(childPrefix, i == len(n.children)-1)
-	}
+	return nil
 }
 
-type Group struct {
-	index int
-	nodes []*Node
-}
-
+// Создание дерева на основании входных данных.
+// На выходе получаем указатель на вершину.
 func CreateTree(nodeCount int, nodesWeights []int, nodesParents []int) *Node {
 	if nodeCount < 1 {
 		return nil
@@ -115,7 +83,124 @@ func CreateTree(nodeCount int, nodesWeights []int, nodesParents []int) *Node {
 	return allNodes[0]
 }
 
-func Solve(root *Node) []int {
-	root.PrintTree()
-	return nil
+// Инициализация групп.
+// Разбиение группы по правилам задачи.
+// Создание групп по количеству листьев дерева.
+func InitGroups(root *Node) []*Group {
+	groups := findSubgroups(root)
+	return groups
+}
+
+// Поиск подгрупп, включая корень.
+// После поиска, корень окажется в одной из подгрупп.
+// Корень попадает в подгруппу для которой он может иметь значение.
+// Если корень перевешивает вес всей подгруппы, то он добавляется в неё.
+func findSubgroups(node *Node) []*Group {
+	subGroups := make([]*Group, len(node.children))
+
+	return subGroups
+}
+
+// Узел дерева.
+type Node struct {
+	index    int
+	weight   int
+	children []*Node
+	parent   *Node
+	group    *Group
+}
+
+// Вывод отладочной информации.
+func (n *Node) PrintTree() {
+	if n == nil {
+		fmt.Println("Tree is nil")
+		return
+	}
+	n.printTreeDetailed("", true)
+}
+
+// Дополнительная функция для вывода древовидного представления.
+func (n *Node) printTreeDetailed(prefix string, isLast bool) {
+	// Определяем символы для визуализации
+	var connector, childPrefix string
+	if isLast {
+		connector = "└── "
+		childPrefix = prefix + "    "
+	} else {
+		connector = "├── "
+		childPrefix = prefix + "│   "
+	}
+
+	// Выводим информацию об узле
+	parentInfo := "nil"
+	if n.parent != nil {
+		parentInfo = fmt.Sprintf("%d", n.parent.index)
+	}
+
+	fmt.Printf("%s%s Node %d (weight=%d, parent=%s, children=%d)\n",
+		prefix, connector, n.index, n.weight, parentInfo, len(n.children))
+
+	// Рекурсивно выводим детей
+	for i, child := range n.children {
+		child.printTreeDetailed(childPrefix, i == len(n.children)-1)
+	}
+}
+
+// Группа.
+type Group struct {
+	index     int
+	nodes     []*Node
+	maxWeight int
+}
+
+// Функция печати информации по каждой отдельной группе.
+func (g *Group) PrintGroup() {
+	if g == nil {
+		fmt.Println("Group is nil")
+		return
+	}
+
+	fmt.Printf("Group %d (nodes count: %d)\n", g.index, len(g.nodes))
+
+	if len(g.nodes) == 0 {
+		fmt.Println("  (empty group)")
+		return
+	}
+
+	// Выводим все узлы группы
+	for i, node := range g.nodes {
+		var prefix string
+		if i == len(g.nodes)-1 {
+			prefix = "└── "
+		} else {
+			prefix = "├── "
+		}
+
+		// Информация о родителе узла
+		parentInfo := "nil"
+		if node.parent != nil {
+			parentInfo = fmt.Sprintf("%d", node.parent.index)
+		}
+
+		// Информация о детях узла
+		childrenInfo := "none"
+		if len(node.children) > 0 {
+			childrenInfo = fmt.Sprintf("%v", getChildrenIndexes(node.children))
+		}
+
+		fmt.Printf("  %sNode %d (weight=%d, parent=%s, children=[%s])\n",
+			prefix, node.index, node.weight, parentInfo, childrenInfo)
+	}
+}
+
+// Вспомогательная функция для получения индексов детей узлов.
+func getChildrenIndexes(children []*Node) string {
+	if len(children) == 0 {
+		return ""
+	}
+	indexes := make([]string, len(children))
+	for i, child := range children {
+		indexes[i] = fmt.Sprintf("%d", child.index)
+	}
+	return strings.Join(indexes, ", ")
 }
